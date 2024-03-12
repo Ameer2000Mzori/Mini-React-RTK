31b2ed58d9b04b6b69c4d6b196770e77b53bddbb
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-03-12 17:44:25 +0100

    added styled component to the project

--- src/component/page/AddProduct.jsx
+++ src/component/page/AddProduct.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Formik } from 'formik'
 import { validationSchema } from './shared/validationSchema.js'
+import { StyledPageWrapper } from './shared/StyledComponents.jsx'
 
 const AddProduct = () => {
   const formik = new Formik({
@@ -17,7 +18,13 @@ const AddProduct = () => {
     validationSchema: validationSchema,
   })
 
-  return <div>AddProduct</div>
+  return (
+    <>
+      <StyledPageWrapper>
+        <div>AddProduct</div>
+      </StyledPageWrapper>
+    </>
+  )
 }
 
 export default AddProduct
---
+++ src/component/page/shared/StyledComponents.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledPageWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
